8519c5c7157cb532df20b86f1dce9ccff46f46dd
Anderson Marques <andersongabriel96.ag@gmail.com>
2018-05-29 11:52:57 -0300

    Criado Service e interface para Cargo, Departamento e Funcionario

---
+++ src/main/java/com/andersonmarques/service/DepartamentoService.java
@@ -0,0 +1,18 @@
+package com.andersonmarques.service;
+
+import com.andersonmarques.domain.Departamento;
+
+import java.util.List;
+
+public interface DepartamentoService {
+
+    void salvar(Departamento cargo);
+
+    void editar(Departamento cargo);
+
+    void excluir(Long id);
+
+    Departamento buscarPorId(Long id);
+
+    List<Departamento> buscarTodos();
+}
---
+++ src/main/java/com/andersonmarques/service/FuncionarioService.java
@@ -0,0 +1,18 @@
+package com.andersonmarques.service;
+
+import com.andersonmarques.domain.Funcionario;
+
+import java.util.List;
+
+public interface FuncionarioService {
+
+    void salvar(Funcionario cargo);
+
+    void editar(Funcionario cargo);
+
+    void excluir(Long id);
+
+    Funcionario buscarPorId(Long id);
+
+    List<Funcionario> buscarTodos();
+}
