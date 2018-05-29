599bd2589d4616f6670760d8ad2dc7d5ff219443
Anderson Marques <andersongabriel96.ag@gmail.com>
2018-05-29 09:18:34 -0300

    Criado Entidade Endereço e enum para UF

---
+++ src/main/java/com/andersonmarques/domain/enums/UF.java
@@ -0,0 +1,46 @@
+package com.andersonmarques.domain.enums;
+
+public enum UF {
+    AC("AC", "Acre"),
+    AL("AL", "Alagoas"),
+    AM("AM", "Amazonas"),
+    BA("BA", "Bahia"),
+    CE("CE", "Ceará"),
+    DF("DF", "Distrito Federal"),
+    ES("ES", "Espírito Santo"),
+    GO("GO", "Goiás"),
+    MA("MA", "Maranhão"),
+    MT("MT", "Mato Grosso"),
+    MS("MS", "Mato Grosso do Sul"),
+    MG("MG", "Minas Gerais"),
+    PA("PA", "Pará"),
+    PB("PB", "Paraíba"),
+    PR("PR", "Paraná"),
+    PE("PE", "Pernambuco"),
+    PI("PI", "Piauí"),
+    RJ("RJ", "Rio de Janeiro"),
+    RN("RN", "Rio Grande do Norte"),
+    RS("RS", "Rio Grande do Sul"),
+    RO("RO", "Rondônia"),
+    RR("RR", "Roraima"),
+    SC("SC", "Santa Catarina"),
+    SP("SP", "São Paulo"),
+    SE("SE", "Sergipe"),
+    TO("TO", "Tocantins");
+
+    private String cod_sigla;
+    private String nome_descricao;
+
+    UF(String cod_sigla, String nome_descricao) {
+        this.cod_sigla = cod_sigla;
+        this.nome_descricao = nome_descricao;
+    }
+
+    public String getCod_sigla() {
+        return cod_sigla;
+    }
+
+    public String getNome_descricao() {
+        return nome_descricao;
+    }
+}
