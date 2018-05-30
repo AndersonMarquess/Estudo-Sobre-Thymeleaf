3bfe2410e6cc7176a8626345399243983252d7db
Anderson Marques <andersongabriel96.ag@gmail.com>
2018-05-30 14:41:35 -0300

    Adicionado funcionadade ao cadastro de Cargos

---
+++ src/main/java/com/andersonmarques/web/controller/CargosController.java
@@ -0,0 +1,47 @@
+package com.andersonmarques.web.controller;
+
+import com.andersonmarques.domain.Cargo;
+import com.andersonmarques.domain.Departamento;
+import com.andersonmarques.service.CargoService;
+import com.andersonmarques.service.DepartamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/cargos")
+public class CargosController {
+
+    @Autowired
+    private CargoService cargoService;
+    @Autowired
+    private DepartamentoService departamentoService;
+
+    @GetMapping("/cadastrar")
+    public String cadastrar(Cargo cargo) {
+        return "/cargo/cadastro";
+    }
+
+    @GetMapping("/listar")
+    public String listar() {
+        return "/cargo/lista";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(Cargo cargo, RedirectAttributes attr) {
+        cargoService.salvar(cargo);
+        attr.addFlashAttribute("success", "Cargo inserido com sucesso!");
+        return "redirect:/cargos/cadastrar";
+    }
+
+    @ModelAttribute("departamentos")
+    public List<Departamento> listaDeDepartamentos() {
+        return departamentoService.buscarTodos();
+    }
+}
